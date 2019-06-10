44348676b8721f9393cbb30df400afca82ebec1c
Mikhail Khrenov <mkhrenov34@gmail.com>
2019-06-09 22:19:23 -0400

    Static/Templates reshuffle

--- autocycle/settings.py
+++ autocycle/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'autocycle.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
