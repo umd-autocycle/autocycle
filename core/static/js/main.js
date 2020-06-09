321d253f479945eb78366296e0157eb1fc846c10
Mikhail Khrenov <mkhrenov34@gmail.com>
2020-06-09 18:34:25 -0400

    Made somewhat mobile friendly, added future section on timeline

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -8,3 +8,23 @@ window.onscroll = function () {
     }
     prevScrollpos = currentScrollPos;
 }
+
+let adjust_top = function () {
+    document.getElementById("content").style.paddingTop = document.getElementById("navbar").offsetHeight + "px";
+    console.log("executed");
+};
+
+window.onload = () => {
+    adjust_top();
+    setTimeout(adjust_top, 150);
+};
+
+window.onresize = () => {
+    setTimeout(adjust_top, 150);
+};
+
+
+document.querySelector("#navbar a").addEventListener("transitionend", () => {
+        setTimeout(adjust_top, 150);
+    }
+);
